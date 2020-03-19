af364cf1596949413dde54b3ccb45341b992b03c
Cristian Santiz <crisantizan@gmail.com>
2020-03-19 17:15:35 -0500

    feat: joi schema

---
+++ src/common/joi-schemas/user.schema.ts
@@ -0,0 +1,15 @@
+import joi from '@hapi/joi';
+import { UserCreate } from '@/endpoints/users/users.type';
+
+/** joi user schema */
+export const userSchema = joi.object<UserCreate>({
+  name: joi.string().required(),
+  surname: joi.string().required(),
+  password: joi.string().required(),
+  email: joi
+    .string()
+    .required()
+    .email(),
+  role: joi.string().required(),
+  image: joi.string().empty(),
+});
