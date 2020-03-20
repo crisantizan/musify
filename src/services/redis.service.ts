9d99ad9d62dfceb1252f8205fc12fbbed76e994e
Cristian Santiz <crisantizan@gmail.com>
2020-03-20 10:57:53 -0500

    feat: add redis service

--- src/services/redis.service.ts
+++ src/services/redis.service.ts
@@ -1,5 +1,18 @@
 import { Tedis } from 'tedis';
+import { EnvService } from './env.service';
 
 export class RedisService extends Tedis {
+  constructor() {
+    const { host, redisPort } = new EnvService();
 
+    super({
+      host,
+      port: redisPort,
+    });
+  }
+
+  /** generate the user key to get his token */
+  public generateUserkey(userId: string) {
+    return { redisUserKey: `userKey${userId}` };
+  }
 }
