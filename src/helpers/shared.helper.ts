71699de3e44fb74da5fd94b1c7264623310c6f77
Cristian Santiz <crisantizan@gmail.com>
2020-03-22 15:07:39 -0500

    feat: add helper generate token

--- src/helpers/shared.helper.ts
+++ src/helpers/shared.helper.ts
@@ -13,3 +13,29 @@ export function errorFieldObject(
 ): ErrorFieldObject {
   return { field, message };
 }
+
+/** generate random string */
+function generateToken(length: number = 4, addDate = false): string {
+  let randomString = '';
+  const charts =
+    'aAbB0cCd#DeE1fFgGh2HiIjJ3kK$l4LmM5nNoOp6Pq-QrR7sStT8uUv_VwW9xXyYzZ';
+
+  while (randomString.length < length) {
+    randomString += charts.charAt(Math.round(Math.random() * charts.length));
+  }
+
+  if (addDate) {
+    const date = new Date();
+    const dateString = date.toISOString().substr(0, 10);
+
+    const time = date
+      .toUTCString()
+      .match(/[0-9][0-9]:[0-9][0-9]:[0-9][0-9]/)![0]
+      .split(':')
+      .join('-');
+
+    randomString = `${dateString}T${time}_${randomString}`;
+  }
+
+  return randomString;
+}
