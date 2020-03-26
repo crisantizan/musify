f528768b1a9ceb575bc35fedff847bd534c97c78
Cristian Santiz <crisantizan@gmail.com>
2020-03-25 20:47:27 -0500

    chore: add index service file

---
+++ src/services/index.ts
@@ -0,0 +1,5 @@
+export { EncryptService } from './encrypt.service';
+export { EnvService } from './env.service';
+export { JwtService } from './jwt.service';
+export { RedisService } from './redis.service';
+export { Service } from './service';
